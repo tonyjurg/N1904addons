a6c910a6c93129a640dc33ebb3f71dd461471321
tonyjurg <github@jurgnet.nl>
2025-05-23 18:02:27 +0200

    Create osis_id.tf

---
+++ tf/1.0.0/osis_id.tf
@@ -0,0 +1,42 @@
+@node
+@author=OSIS initiative
+@convertedBy=Tony Jurg
+@coreData=Nestle 1904 Text-Fabric (centerBLC)
+@coreDataUrl=https://github.com/CenterBLC/N1904
+@description=OSIS is an XML Schema definition for Bibles and other Biblical research texts.
+@license=MIT License
+@licenseUrl=https://mit-license.org/
+@provenance=jupyter Notebook (https://github.com/tonyjurg/create_TF_feature_betacode)
+@valueType=str
+@version=1.0.0
+@website=https://github.com/tonyjurg/N1904addon
+@writtenBy=Text-Fabric
+@dateWritten=2025-05-23T15:45:42+00:00Z
+
+137780	Matt
+Mark
+Luke
+John
+Acts
+Rom
+1Cor
+2Cor
+Gal
+Eph
+Phil
+Col
+1Thess
+2Thess
+1Tim
+2Tim
+Titus
+Phlm
+Heb
+Jas
+1Pet
+2Pet
+1John
+2John
+3John
+Jude
+Rev
